307cd467e72243ca8ec1cca585447a88ac684368
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2025-01-18 14:34:56 +0600

    Create 1005 - Average 1.cpp

---
+++ 1005 - Average 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float A, B, average;
+    cin >> A;
+    cin >> B;
+
+    average = (((A * 3.5) + (B * 7.5)) / (3.5 + 7.5));
+    cout << "MEDIA = " << fixed << setprecision(5) << average << endl;
+
+    return 0;
+}
